365df61d16b0a9af99851f06f969f930406d53d8
Harisshks <harisshhari8@gmail.com>
2024-09-19 22:38:58 +0530

    updated profile

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -5,21 +5,21 @@ const Navbar = () => {
   return (
     <>
     
-<div className='w-full h-[3rem] flex justify-center items-center bg-blue-500 shadow-md shadow-blue-700 rounded-sb' >
+<div className='w-full h-[4rem] flex justify-center items-center bg-blue-400 shadow-md shadow-blue-700 rounded-sb' >
 <div className='w-[95%] h-full flex justify-center items-center flex-row'>
 <div className='w-1/2  h-full flex justify-start items-center font-bold text-xl ☐ text-white'> Harissh K S</div>
 <div className='w-1/2  h-full flex justify-end items-center'>
 <ul className='flex list-none flex-row justify-center gap-6 ml-auto '>
               <Link to='/'>
-                <li className='w-1/2  h-full font-bold text-xl text-white hover:opacity-80'>Profile</li>
+                <li className='w-1/2  h-full font-bold text-xl text-white hover:opacity-50'>Profile</li>
               </Link>
               <Link to='/project'>
-                <li className='w-1/2  h-full font-bold text-xl text-white hover:opacity-80'>Project</li>
+                <li className='w-1/2  h-full font-bold text-xl text-white hover:opacity-50'>Project</li>
               </Link>
               <Link to='/contact'>
-                <li className='w-1/2  h-full font-bold text-xl text-white hover:opacity-80'>Contact</li>
+                <li className='w-1/2  h-full font-bold text-xl text-white hover:opacity-50'>Contact</li>
               </Link>
-              <CircleUser size={28} color="#f4f0f0" className='w-1/2  h-full hover:opacity-80'/>
+              <CircleUser size={28} color="#f4f0f0" className='w-1/2  h-full hover:opacity-50'/>
               </ul>
 </div> 
 </div>
--- portfolio/src/pages/Profile.jsx
+++ portfolio/src/pages/Profile.jsx
@@ -1,10 +1,21 @@
 import Navbar from '../components/Navbar'
+import myim from '../assests/img/myim.jpg'
 const Profile = () => {
   return (
     <>
       <Navbar/>
-      <div>Profile</div>
-      </>
+      <div className='h-full w-full  bg-blue-100 bg-opacity-40 flex  flex-col items-center'>
+      <div className='h-[80%] w-screen flex justify-center'>
+            <div className='h-screen w-[60%] flex flex-col justify-start my-20 items-center ml-6'>
+                <div className=' text-black  w-[50%] h-[10%] my-10 font-semibold text-5xl flex justify-center items-center'>Heyy! I'am Harissh</div>
+                <div className=' w-[80%]  text-black h-[20%] my-5 font-thin text-3xl justify-center items-center'>Creative and detail-oriented college student with a passion for coding and storytelling. Experienced in Android development and YouTube content creation, with a focus on gaming and animated videos. Constantly learning and adapting to new technologies to bring innovative ideas to life.</div>
+            </div>
+            <div className='h-screen w-[40%] flex justify-center items-start my-28'>
+                <div className='h-screen w-[40%]  rounded-full'><img src={myim} className='rounded-full border-2 border-r-blue-600'></img></div>
+            </div>
+      </div>
+        </div>
+        </>
     )
 }
 
