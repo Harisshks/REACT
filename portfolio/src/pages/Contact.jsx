ae9ac824bbb4b1838d044637aefda2996187d858
Harisshks <harisshhari8@gmail.com>
2024-09-23 00:12:53 +0530

    updated navbar

--- portfolio/src/pages/Contact.jsx
+++ portfolio/src/pages/Contact.jsx
@@ -2,33 +2,61 @@ import React, { useState } from 'react'
 import Navbar from '../components/Navbar'
 
 const Contact = () => {
-  const [visible,setvisible] =useState(false)
   return (
     <>
-    {
-      visible &&(
-        <>
-    <div className="h-screen w-screen absolute top-0 left-0 bg-black/20 flex justify-center items-center z-50">
-    <div className="h-[40%] w-[30%] bg-red z-50 flex flex-col bg-white">
-      <div className="w-full h-[20%] flex flex-row justify-center items-center border-2">
-        Delete?
-      </div>
-    <div className="w-full h-[60%] flex flex-row justify-center items-center">
-      Are you sure?
-    </div>
-    <div className="w-full h-[20%] flex justify-center items-center">
-    <button onClick={()=> setvisible(!visible)} className="bg-red-500 p-3 text-white w-1/2 h-full rounded-sm">No</button>
-    <button onClick={()=> setvisible(!visible)} className="bg-green-500 p-3 text-white w-1/2 h-full rounded-sm">Yes</button>
-    </div>
-    </div>
-    </div>
+    <Navbar/>
+    import React from 'react'
     </>
   )
 }
-<div className="w-full h-[20%] flex flex-row justify-center items-center -z-10">
-<button onClick={()=> setvisible(true)} className="bg-red-500 p-3 text-white w-1/2 h-full rounded-sm">Delete</button>
+export default Contact
+
+
+// import { Github, Linkedin, LinkedinIcon, LucideLinkedin, Mail, Phone } from 'lucide-react'
+
+
+// const Contact = () => {
+//   return (
+
+  
+//     <div className='h-screen w-screen bg-teal-950 flex justify-around items-center'>
+//       <div className='font-bold text-slate-50 *flex justify-start  items-start h-[80%] w-[10%] text-5xl'><h1>CONTACT</h1></div>
+//           <div><a href="mailto:jsakshaykarthik@gmail.com"><Mail className='text-white h-10 w-10 hover:animate-bounce' />MAIL</a></div>
+//           <div><a href="https://www.linkedin.com/in/akshay-karthik-s-041a26253/?originalSubdomain=in" target="_blank"><Linkedin className='text-white h-10 w-10 hover:animate-bounce' />LINKEDIN</a></div>
+//           <div><a href="https://github.com/Akshaykarthik22" target="_blank"><Github className='text-white h-10 w-10 hover:animate-bounce'/>GITHUB</a></div>
+//           <div><a href="tel:+917010160414"><Phone className='text-white h-10 w-10 hover:animate-bounce'/>PHONE</a></div>
+//         </div>
+
+      
+    
+
+
+//         )
+// }
+
+// export default Contact
+
+// const [visible,setvisible] =useState(false)
+{/* {
+  visible &&(
+    <>
+<div className="h-screen w-screen absolute top-0 left-0 bg-black/20 flex justify-center items-center z-50">
+<div className="h-[40%] w-[30%] bg-red z-50 flex flex-col bg-white">
+  <div className="w-full h-[20%] flex flex-row justify-center items-center border-2">
+    Delete?
+  </div>
+<div className="w-full h-[60%] flex flex-row justify-center items-center">
+  Are you sure?
+</div>
+<div className="w-full h-[20%] flex justify-center items-center">
+<button onClick={()=> setvisible(!visible)} className="bg-red-500 p-3 text-white w-1/2 h-full rounded-sm">No</button>
+<button onClick={()=> setvisible(!visible)} className="bg-green-500 p-3 text-white w-1/2 h-full rounded-sm">Yes</button>
+</div>
+</div>
 </div>
 </>
-  )
-}
-export default Contact
+) */}
+{/* }
+<div className="w-full h-[20%] flex flex-row justify-center items-center -z-10">
+<button onClick={()=> setvisible(true)} className="bg-red-500 p-3 text-white w-1/2 h-full rounded-sm">Delete</button>
+</div> */}
