00646cf2d0402f0afcd68ad0b586a721a76cb5d4
Harisshks <harisshhari8@gmail.com>
2024-09-24 22:25:32 +0530

    Updated files

--- portfolio/src/pages/Project.jsx
+++ portfolio/src/pages/Project.jsx
@@ -3,24 +3,29 @@ import '../assests/css/card.css';
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import gameImage from '../assests/img/game.jpg'; 
+import gameImage from '../assests/img/port1.jpeg'; 
+import calculator from '../assests/img/Calculator.png';
+import rspimage from '../assests/img/rockps1.png';
 import Navbar from '../components/Navbar';
 
 const data = [
   {
-    name: '3D-FPS Game',
+    name: 'React-Portfolio',
     img: gameImage, 
-    about: 'Project 1'
+    about: 'Explore my interactive portfolio built with React, showcasing projects, skills. Dive into a dynamic and responsive experience that highlights my work.',
+    link:'https://github.com/Harisshks/REACT'
   },
   {
-    name: 'Hari',
-    img: gameImage, // Use imported image
-    about: 'Project 1'
+    name: 'Basic Calculator',
+    img: calculator, 
+    about: 'Developed a simple yet efficient calculator app using Kotlin, handling basic arithmetic operations. The app features with seamless functionality for everyday calculations',
+    link:'https://github.com/Harisshks/Basiccalculator'
   },
   {
-    name: 'Harkissh',
-    img: gameImage, // Use imported image
-    about: 'Project 1'
+    name:'RockPaperScissor',
+    img: rspimage, 
+    about: 'Created an interactive Rock-Paper-Scissors game using Kotlin, with intuitive gameplay. The app offers a fun and simple way to challenge the computer with a classic game.',
+    link:'https://github.com/Harisshks/RockPaperScissor'
   }
 ];
 
@@ -37,11 +42,11 @@ const Project = () => {
   return (
     <>
     <Navbar/>
-      <div className='w-3/4 m-auto'>
+      <div className='w-3/4 m-auto '>
         <div className='mt-20'>
           <Slider {...settings}>
             {data.map((g, index) => (
-              <div key={index} className='bg-white h-[400px] text-black rounded-xl'>
+              <div key={index} className='bg-white h-[400px] text-black rounded-xl transform hover:scale-105 hover:shadow-[0_0_20px_rgba(255,255,255,0.6)] transition-transform duration-300'>
                 <div className='rounded-t-xl flex justify-center items-center'>
                   <img src={g.img} alt={g.name} className='rounded-t-xl h-full w-full' />
                 </div>
@@ -49,7 +54,9 @@ const Project = () => {
                 <div className='flex flex-col justify-center items-center gap-4 p-4'>
                   <p className='text-xl font-semibold'>{g.name}</p>
                   <p>{g.about}</p>
-                  {/* <button className='bg-black text-white text-lg px-6 py-1 rounded-xl'>Source</button> */}
+                </div>
+                <div className='flex items-end justify-center'>
+                  <div className='btn bg-orange-500 text-white text-lg px-6 py-1 rounded-xl hover:opacity-60'><a href={g.link} target="_blank">View</a></div>
                 </div>
               </div>
             ))}
